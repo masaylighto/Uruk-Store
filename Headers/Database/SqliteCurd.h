b7c2ea6cf59618ffb5efddb1e300f3ee3262e1e8
Masaylighto <www.masaylighto@gmail.com>
2022-06-14 22:44:34 +0300

    start implementing sqlite function

--- Headers/Database/SqliteCurd.h
+++ Headers/Database/SqliteCurd.h
@@ -2,12 +2,16 @@
 #include <vector>
 #include<string>
 #include <vector>
-#include <sqlite3.h>
+#include "Helper/Apt.h"
 #include <stdexcept>
+#include <cstring>
+#include <sqlite3.h>
 #ifndef H_SqliteCurd
 #define H_SqliteCurd
+
 class SqliteCurd
 {
+  
     SqliteCurd(std::string Path);
     SqliteCurd()=default;
     sqlite3 * Db;
@@ -15,12 +19,17 @@ class SqliteCurd
     this method create static instance of the class and return it, this method can be used instead of constructing the class every time
     */
     static SqliteCurd * Create(std::string Path);
+  
+   
+    sqlite3_stmt * Execute(const char * SqlQuery ,std::vector<std::string> & SqlParameters);
+    sqlite3_stmt * Execute(const char * SqlQuery);
+    sqlite3_stmt * CreateStatement(const char * SqlQuery );
+    void FreeStdVector(std::vector<std::string> & Vec);
+    void BindParameters(sqlite3_stmt * Stmt,std::vector<std::string> & SqlParameters);
     public :
-    template <typename Type> Type FetchRow (std::string SqlQuery);
-    template <typename Type> Type FetchList (std::string SqlQuery);
-    template <typename Type> std::vector<Type> FetchRow (std::string SqlQuery);
-    template <typename Type> bool AddRow (std::string SqlQuery,std::vector<Type> Parameters);
-    template <typename Type> bool Delete (std::string SqlQuery,std::vector<Type> Parameters);
-    template <typename Type> bool Update (std::string SqlQuery,std::vector<Type> Parameters);
+    std::vector<std::string> GetPackagesName ();
+    void DeletePackages ();
+    void AddPackage (Package Pkg);
+    
 };
 #endif
--- Source/Database/SqliteCurd.cpp
+++ Source/Database/SqliteCurd.cpp
@@ -4,37 +4,96 @@ SqliteCurd::SqliteCurd(std::string Path)
 {
   if (SQLITE_OK!=sqlite3_open(Path.c_str(),&Db))
   {
-    throw new std::runtime_error("couldn't open the file");
+    throw new std::runtime_error("couldn't open the file "+std::string(sqlite3_errmsg(Db)));
   }
     
 }
 SqliteCurd * SqliteCurd::Create(std::string Path)
 {
-    static SqliteCurd * instance;
-    if (!instance)
+    static SqliteCurd * Instance;
+    if (!Instance)
     {
-        instance = new SqliteCurd(Path);
+        Instance = new SqliteCurd(Path);
     }    
-    return instance;
+    return Instance;
 
+}
+
+std::vector<std::string> SqliteCurd::GetPackagesName ()
+{
+  std::vector<std::string> PackagesName;
+  sqlite3_stmt* Stmt=Execute("Select Name from Packages");
+  for (int index = 0; index <  sqlite3_column_count(Stmt); index++)
+  {
+    PackagesName.push_back((char*)sqlite3_column_text(Stmt,0));
+  } 
+  sqlite3_finalize(Stmt);
+  return PackagesName;
 }
 /*
-template <typename Type> Type SqliteCurd::FetchRow (std::string SqlQuery){
+this method free Sql Parameters 
+*/
+sqlite3_stmt * SqliteCurd::Execute(const char * SqlQuery ,std::vector<std::string> & SqlParameters)
+{
+  sqlite3_stmt *Stmt =CreateStatement(SqlQuery);  
+  BindParameters(Stmt,SqlParameters);
+  if (sqlite3_step(Stmt)!=SQLITE_DONE)
+  {    
+    throw new std::runtime_error("Failed To Execute Query");
+  }
+//  FreeStdVector(SqlParameters);
+  return Stmt;
 
 }
-template <typename Type> Type SqliteCurd::FetchList (std::string SqlQuery){
-    
+void SqliteCurd::FreeStdVector(std::vector<std::string> & Vec)
+{
+  for (int i = 0; i < Vec.size(); i++)
+  {
+    auto & item =Vec.at(i);
+   
+  }
+  
+  Vec.clear();
 }
-template <typename Type> std::vector<Type> SqliteCurd::FetchRow (std::string SqlQuery){
+void  SqliteCurd::BindParameters(sqlite3_stmt * Stmt,std::vector<std::string> & SqlParameters)
+{
+  for (int index = 0; index < SqlParameters.size(); index++)
+  {
+    std::string & Parameter=SqlParameters.at(index);
+     sqlite3_bind_text(Stmt ,index,Parameter.c_str(),Parameter.length(),nullptr);
+  }
+}
+sqlite3_stmt * SqliteCurd::CreateStatement(const char * SqlQuery){
+  //Object Pointer For prepared statement
+  sqlite3_stmt *Stmt;
+  //the part of our query that the prepare statement left it with no use
+  const char* UnUsedPartSqlQuery;
+  sqlite3_prepare(Db,SqlQuery,std::strlen(SqlQuery),&Stmt,&UnUsedPartSqlQuery); 
+  return Stmt;
 
 }
-template <typename Type> bool SqliteCurd::AddRow (std::string SqlQuery,std::vector<Type> Parameters){
+sqlite3_stmt * SqliteCurd::Execute(const char * SqlQuery)
+{
+  //Object Pointer For prepared statement
+  sqlite3_stmt *Stmt =CreateStatement(SqlQuery);  
+  if (sqlite3_step(Stmt)!=SQLITE_DONE)
+  {    
+    throw new std::runtime_error("Failed To Execute Query");
+  }
+  return Stmt;
 
 }
-template <typename Type> bool SqliteCurd::Delete (std::string SqlQuery,std::vector<Type> Parameters){
-
+void SqliteCurd::DeletePackages()
+{
+  Execute("delete from Packages");
 }
-template <typename Type> bool SqliteCurd::Update (std::string SqlQuery,std::vector<Type> Parameters){
+void SqliteCurd::AddPackage (Package Pkg)
+{
 
-
-}*/
+  std::vector<std::string>  SqlParameters;
+  SqlParameters.push_back(Pkg.Name);
+  SqlParameters.push_back(Pkg.Section);
+  SqlParameters.push_back(Pkg.Version);
+  SqlParameters.push_back(Pkg.Maintainer);
+  Execute("Insert into Packages (Name,Section,Version,Maintainer) values (?,?,?,?),",SqlParameters);
+}
